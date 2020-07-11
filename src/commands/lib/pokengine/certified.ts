410051120c53b2cebbfc0d2eb10df72312545d1c
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2020-07-11 18:09:04 -0500

    Allow given Pokemon name or dex number.

--- src/commands/lib/pokengine/certified.ts
+++ src/commands/lib/pokengine/certified.ts
@@ -19,7 +19,7 @@ interface WebScrapedFakemon {
 
 export class CertifiedCommand implements Command {
     public names = ['certified'];
-    public args = '(pok\u00E9dex)';
+    public args = '(pok\u00E9dex) (pok\u00E9mon | number)';
     public description = 'Returns a random Fak\u00E9mon from the certified Pok\u00E9dexes on the Pok\u00E9ngine website.';
     public category = CommandCategory.Pokengine;
     public permission = CommandPermission.Everyone;
@@ -63,17 +63,35 @@ export class CertifiedCommand implements Command {
             });
         });
 
+
+
+        let chosenMon: WebScrapedFakemon = null;
+        if (args.length > 1) {
+            // User gave a dex number
+            let dexNum = parseInt(args[1]);
+            if (!isNaN(dexNum) && dexNum > 0 && dexNum < mons.length && mons[dexNum - 1].imagePath) {
+                chosenMon = mons[dexNum - 1];
+            }
+            // User gave a Fakemon name
+            else {
+                const name = args[1].toUpperCase();
+                chosenMon = mons.find(mon => mon.text.toUpperCase().includes(name));
+            }
+        }
+
         // Get a random Fakemon with a front sprite
-        mons = mons.filter(mon => mon.imagePath && !mon.imagePath.endsWith('unknown.png'));
-        const randomMon = mons[Math.floor(Math.random() * mons.length)];
+        if (!chosenMon || !chosenMon.imagePath) {
+            mons = mons.filter(mon => mon.imagePath && !mon.imagePath.endsWith('unknown.png'));
+            chosenMon = mons[Math.floor(Math.random() * mons.length)];
+        }
 
         // Send embed
         const embed = new MessageEmbed();
         embed.setColor(SpindaColors.spots.base.hexString);
         embed.setAuthor(dex.name, dex.iconPath ? this.baseUrl + '/' + dex.iconPath : undefined);
-        embed.setTitle(randomMon.text);
-        embed.setURL(this.baseUrl + randomMon.dexPath);
-        embed.setImage(this.baseUrl + randomMon.imagePath);
+        embed.setTitle(chosenMon.text);
+        embed.setURL(this.baseUrl + chosenMon.dexPath);
+        embed.setImage(this.baseUrl + chosenMon.imagePath);
         embed.setFooter(bot.name, bot.iconUrl);
 
         msg.channel.send(embed);
