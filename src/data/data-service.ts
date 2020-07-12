6f35563964a75e99eade3fd8527b81206654746f
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2020-07-11 21:57:47 -0500

    Change default prefix.

--- src/data/data-service.ts
+++ src/data/data-service.ts
@@ -1,8 +1,10 @@
 import { GuildConfig } from './model/guild-config';
 
 export class DataService {
+    public readonly defaultPrefix: string = '>';
+
     // TODO: Set up some sort of database for guild-specific settings
     public getGuildConfig(guildId: string): GuildConfig {
-        return { prefix: '!' };
+        return { prefix: this.defaultPrefix };
     }
 }
