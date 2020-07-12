53ec82cc11e539bbdd889d6b1c3c15d82a3db9a4
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2020-07-11 19:38:40 -0500

    Fix encoding and error handling.

--- src/commands/lib/pokengine/certified.ts
+++ src/commands/lib/pokengine/certified.ts
@@ -12,7 +12,8 @@ interface WebScrapedPokedex {
 }
 
 interface WebScrapedFakemon {
-    text: string;
+    num: number;
+    name: string;
     dexPath: string;
     imagePath: string;
 }
@@ -25,14 +26,19 @@ export class CertifiedCommand implements Command {
     public permission = CommandPermission.Everyone;
 
     public readonly baseUrl: string = 'http://pokengine.org';
-    public readonly pokedexPath: string = '/pok%C3%A9dex';
+    public readonly pokedexPath: string = '/pok\u00E9dex';
     public certifiedDexNames: WebScrapedPokedex[];
+
+    private formatNum(num: number, length: number = 3): string {
+        const str = num.toString();
+        return str.length < length ? '0'.repeat(length - str.length) + str : str;
+    }
 
     public async run(bot: DiscordBot, msg: Message, args: string[]) {
         // Retrieve certified dex information
         // We cache this data since it is very unlikely to change
         if (!this.certifiedDexNames) {
-            const dexesResponse = await axios.get(this.baseUrl + this.pokedexPath);
+            const dexesResponse = await axios.get(encodeURI(this.baseUrl + this.pokedexPath), { responseEncoding: 'binary' } as any);
             this.certifiedDexNames = [];
             cheerio(dexesResponse.data).find('.dexes').first().find('a.button').each((i, button) => {
                 const ctx = cheerio(button);
@@ -45,42 +51,61 @@ export class CertifiedCommand implements Command {
         }
 
         // Get dex from first argument, or choose a random one
-        const dex = this.certifiedDexNames.find(dex => dex.name.localeCompare(args[0], undefined, { sensitivity: 'base' }) === 0)
-            || this.certifiedDexNames[Math.floor(Math.random() * this.certifiedDexNames.length)];
+        let dex: WebScrapedPokedex = null;
+        if (args.length > 0) {
+            dex = this.certifiedDexNames.find(dex => dex.name.localeCompare(args[0], undefined, { sensitivity: 'base' }) === 0);
+            if (!dex) {
+                throw new Error(`Pok\u00E9dex "${args[0]}" does not exist or is not certified.`);
+            }
+        }
+        else {
+            dex = this.certifiedDexNames[Math.floor(Math.random() * this.certifiedDexNames.length)];
+        }
 
         // Get dex page, this is some what of a slow operation
-        const dexResponse = await axios.get(this.baseUrl + dex.dexPath + '?all');
+        const dexResponse = await axios.get(this.baseUrl + dex.dexPath + '?all', { responseEncoding: 'binary' } as any);
 
         // Gather data
         // TODO: Think about caching this data
         let mons: WebScrapedFakemon[] = [];
         cheerio(dexResponse.data).find('.dex-block').each((i, block) => {
             const ctx = cheerio(block);
-            mons.push({ 
-                text: ctx.text(),
-                dexPath: ctx.attr('href'),
-                imagePath: ctx.find('img').attr('data-src')
-            });
+            const split = ctx.text().split(' ');
+            if (split.length > 1) {
+                mons.push({ 
+                    num: parseInt(split[0]),
+                    name: split.slice(1).join(' '),
+                    dexPath: ctx.attr('href'),
+                    imagePath: ctx.find('img').attr('data-src')
+                });
+            }
         });
 
-
+        if (mons.length === 0) {
+            throw new Error(`Pok\u00E9dex "${dex.name}" is empty.`);
+        }
 
         let chosenMon: WebScrapedFakemon = null;
         if (args.length > 1) {
             // User gave a dex number
             let dexNum = parseInt(args[1]);
-            if (!isNaN(dexNum) && dexNum > 0 && dexNum < mons.length && mons[dexNum - 1].imagePath) {
-                chosenMon = mons[dexNum - 1];
+            if (!isNaN(dexNum)) {
+                chosenMon = mons.find(mon => mon.num === dexNum);
+                if (!chosenMon) {
+                    throw new Error(`#${dexNum} is out of range or private for Pok\u00E9dex ${dex.name}.`);
+                }
             }
             // User gave a Fakemon name
             else {
-                const name = args[1].toUpperCase();
-                chosenMon = mons.find(mon => mon.text.toUpperCase().includes(name));
+                chosenMon = mons.find(mon => mon.name.localeCompare(args[1], undefined, { sensitivity: 'base' }) === 0);
+                if (!chosenMon) {
+                    throw new Error(`Pok\u00E9mon "${args[1]}" does not exist in Pok\u00E9dex ${dex.name}.`);
+                }
             }
         }
 
         // Get a random Fakemon with a front sprite
-        if (!chosenMon || !chosenMon.imagePath) {
+        if (!chosenMon) {
             mons = mons.filter(mon => mon.imagePath && !mon.imagePath.endsWith('unknown.png'));
             chosenMon = mons[Math.floor(Math.random() * mons.length)];
         }
@@ -89,8 +114,8 @@ export class CertifiedCommand implements Command {
         const embed = new MessageEmbed();
         embed.setColor(SpindaColors.spots.base.hexString);
         embed.setAuthor(dex.name, dex.iconPath ? this.baseUrl + '/' + dex.iconPath : undefined);
-        embed.setTitle(chosenMon.text);
-        embed.setURL(this.baseUrl + chosenMon.dexPath);
+        embed.setTitle(`#${this.formatNum(chosenMon.num)} ${chosenMon.name}`);
+        embed.setURL(decodeURI(this.baseUrl + chosenMon.dexPath));
         embed.setImage(this.baseUrl + chosenMon.imagePath);
         embed.setFooter(bot.name, bot.iconUrl);
 
