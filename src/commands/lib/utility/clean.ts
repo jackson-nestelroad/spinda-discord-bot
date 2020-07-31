19ff57679c99e098180d0b00bfb0bd21b8f65ee9
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2020-07-30 23:02:49 -0500

    Cannot delete 2 week old messages.

--- src/commands/lib/utility/clean.ts
+++ src/commands/lib/utility/clean.ts
@@ -10,6 +10,7 @@ export class CleanCommand implements Command {
     public permission = CommandPermission.Administrator;
 
     public readonly defaultNumberToDelete: number = 100;
+    public readonly ageLimit: number = 14 * 24 * 60 * 60 * 1000;
 
     public async run(bot: DiscordBot, msg: Message, args: string[]) {
         let numberToDelete = parseInt(args[0]);
@@ -18,14 +19,19 @@ export class CleanCommand implements Command {
         }
 
         const channelHistory = await msg.channel.messages.fetch({ limit: 100 });
-        const toDelete = channelHistory.filter(msg => msg.author.id === bot.client.user.id).array().slice(0, numberToDelete);
+        const now = new Date();
+        const toDelete = channelHistory
+            .filter(msg => msg.author.id === bot.client.user.id && (now as any) - (msg.createdAt as any) < this.ageLimit)
+            .array().slice(0, numberToDelete);
         
+
         try {
             await msg.channel.bulkDelete(toDelete);
         } catch (error) {
             if (error.message === 'Missing Permissions') {
                 throw new Error('Missing "Manage Messages" permission for this operation.');
             }
+            throw error;
         }
     }
 }
