c7d9c81e0f2c01f65fc4d00f219bf0083dfb3022
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2020-10-15 22:50:59 -0500

    Get code for custom commands.

--- src/commands/lib/utility/custom.ts
+++ src/commands/lib/utility/custom.ts
@@ -1,24 +1,36 @@
 import { Command, CommandCategory, CommandPermission, CommandParameters } from '../base';
 
 export class CustomHelpCommand implements Command {
-    public name = 'custom';
+    public name = 'custom (command)';
     public args = '';
     public description = 'Gives a list of all custom commands registered in this guild.';
     public category = CommandCategory.Utility;
     public permission = CommandPermission.Everyone;
 
-    public async run({ bot, msg, guild }: CommandParameters) {
+    public async run({ bot, msg, args, guild }: CommandParameters) {
         const commands = await bot.dataService.getCustomCommands(msg.guild.id);
         const embed = bot.createEmbed();
         embed.setAuthor(`Custom Commands for ${msg.guild.name}`, msg.guild.iconURL());
 
-        const keys = Object.keys(commands);
-        if (keys.length === 0) {
-            embed.setDescription('None');
+        if (args.length === 0) {
+            const keys = Object.keys(commands);
+            if (keys.length === 0) {
+                embed.setDescription('None');
+            }
+            else {
+                const list = keys.map(name => `\`${guild.prefix}${name}\``).join(', ');
+                embed.setDescription(list);
+            }
         }
         else {
-            const list = keys.map(name => `\`${guild.prefix}${name}\``).join(', ');
-            embed.setDescription(list);
+            const cmd = args[0];
+            if (commands[cmd]) {
+                embed.setTitle(`${guild.prefix}${cmd}`);
+                embed.setDescription(commands[cmd]);
+            }
+            else {
+                throw new Error(`Custom command \`${cmd}\` does not exist.`);
+            }
         }
 
         await msg.channel.send(embed);
