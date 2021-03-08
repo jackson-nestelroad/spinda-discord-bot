d360917e0968cbe0f8c9fc978ae9966913092eac
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2021-03-08 15:15:59 -0600

    Classes for managing data that expires over time.

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,9 +1,2 @@
 type Dictionary<T> = { [key: string]: T };
 type ReadonlyDictionary<T> = { readonly[key: string]: T };
-
-interface TimedCacheEntry<T> {
-    lastFetched: Date;
-    data: T;
-}
-
-type TimedCache<K, T> = Map<K, TimedCacheEntry<T>>;
