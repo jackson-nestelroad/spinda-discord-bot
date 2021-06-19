c28a9775058640783c9273507fab4423b9a8c094
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2021-06-18 19:07:47 -0500

    Two admins must confirm universal commands

--- src/commands/lib/config/run-custom.ts
+++ src/commands/lib/config/run-custom.ts
@@ -1,6 +1,8 @@
 import { CommandCategory, CommandPermission, CommandParameters, ArgumentsConfig, ArgumentType, LegacyCommand, ChatCommandParameters } from '../base';
 import { CustomCommandEngine } from '../../../events/util/custom-command';
 import { ExpireAgeConversion } from '../../../util/timed-cache';
+import { GuildMember, MessageActionRow, MessageButton, MessageComponentInteraction, Permissions } from 'discord.js';
+import { EmbedTemplates } from '../../../util/embed';
 
 interface RunCustomArgs {
     code: string;
@@ -50,11 +52,58 @@ export class RunCustomCommand extends LegacyCommand<RunCustomArgs> {
         return parsed as RunCustomArgs;
     }
 
-    public async run(params: CommandParameters, args: RunCustomArgs) {
+    public async run({ bot, src, guild }: CommandParameters, args: RunCustomArgs) {
+        if (args.universal) {
+            const members = await bot.memberListService.getMemberListForGuild(guild.id);
+            const otherAdmins = members.filter(member => member.permissions.has('ADMINISTRATOR') && !member.user.bot);
+            otherAdmins.delete(src.author.id);
+
+            if (otherAdmins.size > 0) {
+                const row = new MessageActionRow();
+                const button = new MessageButton();
+                button.setCustomID('confirm');
+                button.setLabel('Run Universal Command');
+                button.setEmoji(`\u{2755}`);
+                button.setStyle('DANGER');
+                row.addComponents(button);
+    
+                let response = await src.reply({
+                    content: 'Running a universal command is an extremely dangerous action. Please have another Administrator confirm this command.',
+                    components: [row],
+                });
+
+                if (!response.isMessage) {
+                    throw new Error(`Response to universal command should have produced a message.`);
+                }
+
+                const disableButton = async () => {
+                    const updatedRow = new MessageActionRow();
+                    button.setDisabled(true);
+                    updatedRow.addComponents(button);
+                    response = await response.edit({ components: [updatedRow] });
+                };
+
+                let interaction: MessageComponentInteraction;
+                try {
+                    interaction = await response.message.awaitMessageComponentInteraction(interaction => {
+                        return interaction.customID === 'confirm' && otherAdmins.has(interaction.member.user.id);
+                    }, { time: 60 * 1000 });
+                } catch (error) {
+                    await disableButton();
+                    const embed = bot.createEmbed(EmbedTemplates.Error);
+                    embed.setDescription('Universal command confirmation timed out.');
+                    await src.reply({ embeds: [embed] });
+                    return;
+                }
+
+                await disableButton();
+                await interaction.reply(`Confirmation given by ${interaction.member.toString()}.`);
+            }
+        }
         await new CustomCommandEngine({
-            bot: params.bot,
-            src: params.src,
-            guild: params.guild,
+            bot: bot,
+            src: src,
+            guild: guild,
         }, 'content', [], { 
             universal: args.universal,
         }).run(args.code);
