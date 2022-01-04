442b3b309ce043719cdd9b401402a976be81511f
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2022-01-04 14:42:12 -0600

    Use show args.

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,32 +1,32 @@
-import { CommandTypeArray } from 'panda-discord';
-
 import { BlocklistCommand } from './lib/config/blocklist';
-import { LogsCommand } from './lib/config/logs';
-import { PrefixCommand } from './lib/config/prefix';
-import { RemoveCommandCommand } from './lib/config/remove-command';
-import { RunCustomCommand } from './lib/config/run-custom';
-import { RunUniversalCommand } from './lib/config/run-universal';
-import { SetCommandCommand } from './lib/config/set-command';
-import { SnapshotMembersCommand } from './lib/config/snapshot-members';
-import { ExternalCommands } from './lib/external';
-import { EightBallCommand } from './lib/fun/8ball';
 import { BotsnackCommand } from './lib/fun/botsnack';
 import { ChooseCommand } from './lib/fun/choose';
-import { ColorCommand } from './lib/fun/color';
-import { ConchCommand } from './lib/fun/conch';
-import { ImpostorCommand } from './lib/fun/impostor';
-import { RollCommand } from './lib/fun/roll';
-import { RoutletteCommand } from './lib/fun/roulette';
-import { ScreenshotCommand } from './lib/fun/screenshot';
-import { PokengineCommands } from './lib/pokengine';
-import { EvalCommand } from './lib/secret/eval';
-import { RefreshCommand } from './lib/secret/refresh';
-import { SayCommand } from './lib/secret/say';
-import { SpindaCommands } from './lib/spinda';
 import { CleanCommand } from './lib/utility/clean';
-import { SpindaHelpCommand } from './lib/utility/help';
+import { ColorCommand } from './lib/fun/color';
+import { CommandTypeArray } from 'panda-discord';
+import { ConchCommand } from './lib/fun/conch';
+import { EightBallCommand } from './lib/fun/8ball';
+import { EvalCommand } from './lib/secret/eval';
+import { ExternalCommands } from './lib/external';
+import { ImpostorCommand } from './lib/fun/impostor';
+import { LogsCommand } from './lib/config/logs';
 import { MemoryCommand } from './lib/utility/memory';
 import { PingCommand } from './lib/utility/ping';
+import { PokengineCommands } from './lib/pokengine';
+import { PrefixCommand } from './lib/config/prefix';
+import { RefreshCommand } from './lib/secret/refresh';
+import { RemoveCommandCommand } from './lib/config/remove-command';
+import { RollCommand } from './lib/fun/roll';
+import { RoutletteCommand } from './lib/fun/roulette';
+import { RunCustomCommand } from './lib/config/run-custom';
+import { RunUniversalCommand } from './lib/config/run-universal';
+import { SayCommand } from './lib/secret/say';
+import { ScreenshotCommand } from './lib/fun/screenshot';
+import { SetCommandCommand } from './lib/config/set-command';
+import { ShowArgsCommand } from './lib/secret/show-args';
+import { SnapshotMembersCommand } from './lib/config/snapshot-members';
+import { SpindaCommands } from './lib/spinda';
+import { SpindaHelpCommand } from './lib/utility/help';
 import { UptimeCommand } from './lib/utility/uptime';
 
 export const CommandTypes: CommandTypeArray = [
@@ -54,6 +54,7 @@ export const CommandTypes: CommandTypeArray = [
     EvalCommand,
     RefreshCommand,
     SayCommand,
+    ShowArgsCommand,
 
     PrefixCommand,
     LogsCommand,
