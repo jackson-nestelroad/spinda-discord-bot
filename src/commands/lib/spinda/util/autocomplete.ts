d023945ba7b787311b1c3a00eaa58bedad5d4dae
Jackson Nestelroad <jackson@nestelroad.com>
2023-09-14 20:30:55 -0700

    Filter positions returned by autocomplete

--- src/commands/lib/spinda/util/autocomplete.ts
+++ src/commands/lib/spinda/util/autocomplete.ts
@@ -3,11 +3,13 @@ import { ArgumentAutocompleteFunction } from 'panda-discord';
 import { SpindaGeneratorService } from '../generator';
 
 export const SpindaPositionAutocomplete: ArgumentAutocompleteFunction = context => {
-    return [...Array(SpindaGeneratorService.partySize).keys()].map(i => {
-        const num = i + 1;
-        return {
-            name: num.toString(),
-            value: num,
-        };
-    });
+    return [...Array(SpindaGeneratorService.partySize).keys()]
+        .map(i => {
+            const num = i + 1;
+            return {
+                name: num.toString(),
+                value: num,
+            };
+        })
+        .filter(option => option.name.startsWith(context.value));
 };
